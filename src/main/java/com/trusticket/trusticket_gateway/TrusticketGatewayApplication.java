9816a68394a9c9c1cdf803cfcc5c98f4bea92be7
Jaehwi-So <sjh9708@gmail.com>
2024-06-28 20:36:10 +0900

    feat : Gateway config

--- src/main/java/com/trusticket/trusticket_gateway/TrusticketGatewayApplication.java
+++ src/main/java/com/trusticket/trusticket_gateway/TrusticketGatewayApplication.java
@@ -3,6 +3,13 @@ package com.trusticket.trusticket_gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.reactive.config.CorsRegistry;
 
 @SpringBootApplication
 @EnableDiscoveryClient
